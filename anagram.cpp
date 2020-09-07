9443426234250351c3bc503205d36639ba76d80f
320088280 <320088280@INGBTCPIC5NB405>
2020-09-07 17:30:29 +0530

    Implemented the functions in anagram source file

--- anagram.cpp
+++ anagram.cpp
@@ -1,13 +1,39 @@
 #include "anagram.h"
+#include <cctype>
+#include <algorithm>
 
-bool Anagram::WordPairIsAnagram(const std::string& word1, const std::string& word2) {
-    //Fill the correct implementation here
+bool Anagram::WordPairIsAnagram(const std::string& word1, const std::string& word2)
+{
+	auto word_one = word1;
+	word_one.erase(std::remove_if(word_one.begin(), word_one.end(), ::isspace), word_one.end());
+	
+	auto word_two = word2;
+	word_two.erase(std::remove_if(word_two.begin(), word_two.end(), ::isspace), word_two.end());
+
+	std::transform(word_one.begin(), word_one.end(), word_one.begin(), ::toupper);
+	std::transform(word_two.begin(), word_two.end(), word_two.begin(), ::toupper);
+
+	std::sort(word_one.begin(), word_one.end());
+	std::sort(word_two.begin(), word_two.end());
+
+	if(word_one == word_two)
+	{
+		return true;
+	}
     return false;
 }
 
 std::vector<std::string> Anagram::SelectAnagrams(
         const std::string& word,
-        const std::vector<std::string>& candidates) {
-    //Fill the correct implementation here
-    return candidates;
+        const std::vector<std::string>& candidates)
+{
+	std::vector<std::string> candidateList;
+	for(auto& candidate:candidates)
+	{
+		if(WordPairIsAnagram(word, candidate))
+		{
+			candidateList.push_back(candidate);
+		}
+	}	
+    return candidateList;
 }
